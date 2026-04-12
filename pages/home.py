d1210d659461a6941100ee9c93504d1dc5786b43
MDidoStar <Mohamed.Diaa.Eldin1@gmail.com>
2026-04-12 11:04:36 +0200

    Add Streamlit configuration and header to home page

--- pages/home.py
+++ pages/home.py
@@ -1,3 +1,11 @@
+import streamlit as st
+from datetime import date
+from pymongo.mongo_client import MongoClient
+from color import edit
+
+st.set_page_config(page_title="FLITO: AI Traveling Blogger", page_icon='logo.png', layout="wide")
+
+edit()
 
 
 # --- Header ---
