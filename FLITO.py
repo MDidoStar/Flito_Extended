20c54f8f8d53a5e34c4de17c2bc44d94f81f27cc
MDidoStar <Mohamed.Diaa.Eldin1@gmail.com>
2026-04-12 16:04:11 +0200

    Update page title from 'Blogger' to 'Companion'

--- FLITO.py
+++ FLITO.py
@@ -4,7 +4,7 @@ from datetime import date
 from pymongo.mongo_client import MongoClient
 from color import edit
 
-st.set_page_config(page_title="FLITO: AI Traveling Blogger", page_icon='logo.png', layout="wide")
+st.set_page_config(page_title="FLITO: AI Travel Companion", page_icon='logo.png')
 
 edit()
 
